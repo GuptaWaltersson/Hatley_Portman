d2a16f6666e647666bdf159442068e497f3630ba
GuptaWaltersson <huwa22@student.bth.se>
2025-04-28 21:25:17 +0200

    ja vette fan

--- Application/scripts/hat.lua
+++ Application/scripts/hat.lua
@@ -1,29 +1,56 @@
 local hat = {}
 
+HatType = 
+{
+	DEFAULT = 1,
+	MAGIC = 2,
+	LEGENDARY = 3
 
+}
 
 position = {x=1000,y=500}
 movement = {dx=0.0,dy=0.0,ax = 2400, ay=700}
-
+local hat_tag = 1
 
 -- Behaviours
 function hat.OnCreate(self)
 	print("Hat created!")
 	scene.SetComponent(self.ID,"sprite","../Textures/Portman_hat.png")
-	scene.SetComponent(self.ID,"hattag",1)
+	scene.SetComponent(self.ID,"hattag",hat_tag)
 	scene.SetComponent(self.ID,"position",position)
 	scene.SetComponent(self.ID, "boundingbox", { width = 64, height = 32})
 	scene.SetComponent(self.ID,"movement",movement)
 end
 
 function hat.OnUpdate(delta)
-	--print("Hat updated (:")
+	position.x = position.x + movement.dx
+	print(movement.dx)
+	scene.SetComponent(hat.ID,"position",position)
+	scene.SetComponent(hat.ID,"movement",movement)
+
 end
 
 function hat.throw(delta)
 	print("hat kast")
-	position = {x=800,y=500}
-	scene.SetComponent(hat.ID,"position",position)
+	scene.GetComponent(hat.ID,"hattag",hat_tag)
+	
+    if hat_tag == HatType.DEFAULT then
+        print("Default Hat Behavior")
+		
+    elseif hat_tag == HatType.MAGIC then
+        print("Magic Hat Behavior")
+    elseif hat_tag == HatType.LEGENDARY then
+        print("Legendary Hat Behavior")
+    else
+        print("Unknown hat type:"..hat_tag)
+    end
+
+end
+
+local function defaultThrow()
+	xSpeed = 50
+	movement.dx = xSpeed
+	scene.SetComponent(hat.ID,"movement",movement)
 end
 
 return hat

5ec3460e4fd381161a30862060ed1967351a7493
TothOliver <olivertoth.in@gmail.com>
2025-04-28 11:08:40 +0200

    dirt blocks

--- Application/scripts/block.lua
+++ Application/scripts/block.lua
@@ -1,11 +1,27 @@
 local block = {}
 
 --Components
-local entity = scene.CreateEntity()
+for i = 0, 22 do
+	local entity = scene.CreateEntity()
+	scene.SetComponent(entity, "sprite", "../Textures/tile_0022.png")
+	scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
+	scene.SetComponent(entity, "position", { x = 18+(72*i), y = 764})
+end
 
-scene.SetComponent(entity, "sprite", "../Textures/tile_0000.png")
-scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
-scene.SetComponent(entity, "position", { x = 200, y = 690})
+for i = 0, 22 do
+	local entity = scene.CreateEntity()
+	scene.SetComponent(entity, "sprite", "../Textures/tile_0122.png")
+	scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
+	scene.SetComponent(entity, "position", { x = 18+(72*i), y = 836})
+end
+
+for i = 0, 22 do
+	local entity = scene.CreateEntity()
+	scene.SetComponent(entity, "sprite", "../Textures/tile_0122.png")
+	scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
+	scene.SetComponent(entity, "position", { x = 18+(72*i), y = 908})
+end
+
 
 --Behaviors
 function block.OnCreate(self)

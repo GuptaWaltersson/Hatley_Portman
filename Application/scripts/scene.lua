1df171ccfcfeba0261fd20d933cd7dbfa5b813b2
TothOliver <olivertoth.in@gmail.com>
2025-04-29 14:04:03 +0200

    commented out timer

--- Application/scripts/scene.lua
+++ Application/scripts/scene.lua
@@ -32,21 +32,4 @@ local coinNumberEntity = scene.CreateEntity()
 scene.SetComponent(coinNumberEntity, "behaviour", "scripts/coin/coinNumber.lua")
 
 
-local timerEntity = scene.CreateEntity()
-scene.SetComponent(timerEntity, "behaviour", "scripts/timer/timer.lua")
 
-local timerDisplayEntity = scene.CreateEntity()
-scene.SetComponent(timerDisplayEntity, "behaviour", "scripts/timer/timerDisplay.lua")
-
-local digitEntities = {}
-
-for i = 1, 7 do
-    local digit = scene.CreateEntity()
-    scene.SetComponent(digit, "sprite", "../Textures/Numbers/0.png")
-    scene.SetComponent(digit, "position", { x = 500 + i * 50, y = 30 })
-    table.insert(digitEntities, digit)
-end
-
-scene.SetComponent(timerDisplayEntity, "behaviour", "scripts/timer/timerDisplay.lua")
-local b = scene.GetComponent(timerDisplayEntity, "behaviour")
-b.digits = digitEntities
--- Application/scripts/timer/timer.lua
+++ Application/scripts/timer/timer.lua
@@ -11,3 +11,22 @@ function timer.GetTime()
 end
 
 return timer
+
+--local timerEntity = scene.CreateEntity()
+--scene.SetComponent(timerEntity, "behaviour", "scripts/timer/timer.lua")
+
+--local timerDisplayEntity = scene.CreateEntity()
+--scene.SetComponent(timerDisplayEntity, "behaviour", "scripts/timer/timerDisplay.lua")
+
+--local digitEntities = {}
+
+--for i = 1, 7 do
+--    local digit = scene.CreateEntity()
+--    scene.SetComponent(digit, "sprite", "../Textures/Numbers/0.png")
+--    scene.SetComponent(digit, "position", { x = 500 + i * 50, y = 30 })
+--    table.insert(digitEntities, digit)
+--end
+
+--scene.SetComponent(timerDisplayEntity, "behaviour", "scripts/timer/timerDisplay.lua")
+--local b = scene.GetComponent(timerDisplayEntity, "behaviour")
+--b.digits = digitEntities

f534040c5d7b6e8b09bb9814bbfac161aebfb28e
GuptaWaltersson <huwa22@student.bth.se>
2025-04-30 14:51:51 +0200

    Update block.lua

--- Application/scripts/block.lua
+++ Application/scripts/block.lua
@@ -16,73 +16,65 @@ for i = 0, 22 do
 	scene.SetComponent(entity, "position", { x = 18+(72*i), y = 908})
 end
 
-local cloudLength = 5
-for i = 0, cloudLength do
-    local entity = scene.CreateEntity()
-	scene.SetComponent(entity, "tag", "block")
-  
-    if i == 0 then
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0153.png")
-    elseif i == cloudLength then
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0155.png")
-	else
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0154.png")
-    end
 
-    scene.SetComponent(entity, "boundingbox", { width = 60, height = 30 })
-    scene.SetComponent(entity, "position", { x = 300 + (60 * i), y = 550 })
+local function createCloud(length,posX,posY)
+	for i=0, length do
+		local entity = scene.CreateEntity()
+		scene.SetComponent(entity,"tag","block")
+		if i == 0 then
+			scene.SetComponent(entity,"sprite","../Textures/tile_0153.png")
+		elseif i == length then
+			scene.SetComponent(entity,"sprite","../Textures/tile_0155.png")
+		else
+			scene.SetComponent(entity,"sprite","../Textures/tile_0154.png")
+		end
+
+		scene.SetComponent(entity,"boundingbox",{width=60, height =30})
+		scene.SetComponent(entity,"position",{x=posX +(72*i),y=posY})
+	end
 end
 
-local cloudLength2 = 7
-for i = 0, cloudLength2 do
-    local entity = scene.CreateEntity()
-	scene.SetComponent(entity, "tag", "block")
-  
-    if i == 0 then
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0153.png")
-    elseif i == cloudLength2 then
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0155.png")
-	else
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0154.png")
-    end
 
-    scene.SetComponent(entity, "boundingbox", { width = 60, height = 30 })
-    scene.SetComponent(entity, "position", { x = 500 + (60 * i), y = 150 })
-end
 
-for i = 0, 1 do
+local function createSmallBlock(posX,posY)
 	local entity = scene.CreateEntity()
-	scene.SetComponent(entity, "tag", "block")
-	scene.SetComponent(entity, "sprite", "../Textures/tile_0146.png")
-	scene.SetComponent(entity, "boundingbox", { width = 60, height = 30 })
-	scene.SetComponent(entity, "position", { x = 770 + (300 * i), y = 750 })
-end
-
-local mushroomLength = 6
-local mushroomHeight = 4
-
-for i = 0, mushroomLength do
-    local entity = scene.CreateEntity()
-	scene.SetComponent(entity, "tag", "block")
-  
-    if i == 0 then
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0014.png")
-    elseif i == mushroomLength then
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0015.png")
-	elseif i==mushroomLength/2 then
-		scene.SetComponent(entity, "sprite", "../Textures/tile_0012.png")
-	else
-        scene.SetComponent(entity, "sprite", "../Textures/tile_0013.png")
-    end
-
-    scene.SetComponent(entity, "boundingbox", { width = 60, height = 30 })
-    scene.SetComponent(entity, "position", { x = 1000 + (60 * i), y = 772-(mushroomHeight+1)*72 })
+	scene.SetComponent(entity,"tag","block")
+	scene.SetComponent(entity,"sprite","../Textures/tile_0146.png")
+	scene.SetComponent(entity,"boundingbox",{width = 60, height = 30})
+	scene.SetComponent(entity,"position",{x=posX,y=posY})
 end
 
 
-for i = 0, mushroomHeight do
-	local entity = scene.CreateEntity()
-	scene.SetComponent(entity, "tag", "block")
-	scene.SetComponent(entity, "sprite", "../Textures/tile_0032.png")
-	scene.SetComponent(entity, "position", { x = 1000 + (60 * mushroomLength/2), y = 770 - 72 *i })
+
+local function createMushroom(length,height,posX,posY)
+	for i = 0, length do
+		local entity = scene.CreateEntity()
+		scene.SetComponent(entity,"tag","block")
+		
+		if i == 0 then
+			scene.SetComponent(entity,"sprite","../Textures/tile_0014.png")
+		elseif i == length/2 then
+			scene.SetComponent(entity,"sprite","../Textures/tile_0012.png")
+		elseif i == length then
+			scene.SetComponent(entity,"sprite","../Textures/tile_0015.png")
+		else
+			scene.SetComponent(entity,"sprite","../Textures/tile_0013.png")
+		end
+
+		scene.SetComponent(entity,"boundingbox",{width = 60, height = 30})
+		scene.SetComponent(entity,"position",{x=posX+72*i,y=posY})
+	end
+
+	for i = 0, height do
+		local entity = scene.CreateEntity()
+		scene.SetComponent(entity,"tag","block")
+		scene.SetComponent(entity,"sprite","../Textures/tile_0032.png")
+		scene.SetComponent(entity,"position",{x = posX +(72*length/2),y=posY+72*i})
+	end
 end
+
+createMushroom(6,4,1000,488) --Length, Height, X position, Y position
+createCloud(5,300,550) --Length of cloud, X position, Y position
+createCloud(8,500,170)
+createSmallBlock(770,750) -- X postion, Y position
+createSmallBlock(1070,750)

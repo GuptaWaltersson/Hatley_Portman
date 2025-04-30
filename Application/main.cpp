40ee724f55d5be48bbcaf6e699f146bf5b5438f4
TothOliver <olivertoth.in@gmail.com>
2025-04-30 14:12:01 +0200

    remove usless lua stuff

--- Application/main.cpp
+++ Application/main.cpp
@@ -11,34 +11,9 @@
 #include "ecs/components.hpp"
 #include "ecs/systems.hpp"
 
-void DumpError(lua_State* L)
-{
-	if (lua_gettop(L) && lua_isstring(L, -1))
-	{
-		std::cout << "Lua error: " << lua_tostring(L, -1) << std::endl;
-		lua_pop(L, 1);
-	}
-}
-
-void ConsoleThreadFunction(lua_State* L)
-{
-	std::string input;
-	while (!WindowShouldClose()) 
-	{
-		std::cout << "> ";
-		std::getline(std::cin, input);
-
-		if (luaL_dostring(L, input.c_str()) != LUA_OK);
-		{
-			DumpError(L);
-		}
-	}
-}
-
 void LoadScene(lua_State* L, Scene* scene)
 {
 	scene->Clear();
-
 
 	if (luaL_dofile(L, "scripts/scene.lua") != LUA_OK) {
 		std::cerr << "Lua error: " << lua_tostring(L, -1) << std::endl;
@@ -59,7 +34,7 @@ int main()
 	const int screenWidth = 1600;
 	const int screenHeight = 900;
 	
-	InitWindow(screenWidth, screenHeight, "Hatman");
+	InitWindow(screenWidth, screenHeight, "Hat-Trick");
 
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);

dfd3751310d36476d29a6ad942e2d2f9cf00f502
TothOliver <olivertoth.in@gmail.com>
2025-04-30 09:45:15 +0200

    ground move

--- Application/scripts/block.lua
+++ Application/scripts/block.lua
@@ -5,15 +5,7 @@ for i = 0, 22 do
 	scene.SetComponent(entity, "tag", "block")
 	scene.SetComponent(entity, "sprite", "../Textures/tile_0022.png")
 	scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
-	scene.SetComponent(entity, "position", { x = 18+(72*i), y = 764})
-end
-
-for i = 0, 22 do
-	local entity = scene.CreateEntity()
-	scene.SetComponent(entity, "tag", "block")
-	scene.SetComponent(entity, "sprite", "../Textures/tile_0122.png")
-	scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
-	scene.SetComponent(entity, "position", { x = 18+(72*i), y = 836})
+	scene.SetComponent(entity, "position", { x = 18+(72*i), y =  836})
 end
 
 for i = 0, 22 do
@@ -46,11 +38,11 @@ for i = 0, 1 do
 	scene.SetComponent(entity, "tag", "block")
 	scene.SetComponent(entity, "sprite", "../Textures/tile_0156.png")
 	scene.SetComponent(entity, "boundingbox", { width = 60, height = 30 })
-	scene.SetComponent(entity, "position", { x = 700 + (300 * i), y = 650 })
+	scene.SetComponent(entity, "position", { x = 770 + (300 * i), y = 700 })
 end
 
 local mushroomLength = 6
-local mushroomHeight = 3
+local mushroomHeight = 4
 
 for i = 0, mushroomLength do
     local entity = scene.CreateEntity()
@@ -67,7 +59,7 @@ for i = 0, mushroomLength do
     end
 
     scene.SetComponent(entity, "boundingbox", { width = 60, height = 30 })
-    scene.SetComponent(entity, "position", { x = 1000 + (60 * i), y = 700-(mushroomHeight+1)*72 })
+    scene.SetComponent(entity, "position", { x = 1000 + (60 * i), y = 772-(mushroomHeight+1)*72 })
 end
 
 
@@ -75,5 +67,5 @@ for i = 0, mushroomHeight do
 	local entity = scene.CreateEntity()
 	scene.SetComponent(entity, "tag", "block")
 	scene.SetComponent(entity, "sprite", "../Textures/tile_0032.png")
-	scene.SetComponent(entity, "position", { x = 1000 + (60 * mushroomLength/2), y = 700 - 72 *i })
+	scene.SetComponent(entity, "position", { x = 1000 + (60 * mushroomLength/2), y = 770 - 72 *i })
 end

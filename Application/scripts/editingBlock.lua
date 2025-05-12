c29b9a2f14275db739be6b6f68be94c2fd2e523a
TothOliver <olivertoth.in@gmail.com>
2025-05-12 14:38:00 +0200

    ground fixed

--- Application/scripts/editingBlock.lua
+++ Application/scripts/editingBlock.lua
@@ -18,6 +18,14 @@ function block.Ground()
 		scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
 		scene.SetComponent(entity, "position", { x = 18+(72*i), y =  836})
 	end
+
+	for i = 0, 22 do
+		local entity = scene.CreateEntity()
+		scene.SetComponent(entity, "tag", "block")
+		scene.SetComponent(entity, "sprite", "../Textures/tile_0122.png")
+		scene.SetComponent(entity, "boundingbox", { width = 72, height = 72})
+		scene.SetComponent(entity, "position", { x = 18+(72*i), y = 908})
+	end
 end
 
 cloudCounter = 0

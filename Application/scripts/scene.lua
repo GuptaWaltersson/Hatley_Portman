4137a806419ac0473fea58cc87df1b78cada2706
GuptaWaltersson <huwa22@student.bth.se>
2025-05-19 10:04:43 +0200

    added block to save

--- Application/scripts/scene.lua
+++ Application/scripts/scene.lua
@@ -6,8 +6,11 @@ scene.SetComponent(hatEntity,"behaviour","scripts/hat.lua")
 local playerEntity = scene.CreateEntity()
 scene.SetComponent(playerEntity, "behaviour", "scripts/player.lua")
 
+local cloudEntity = scene.CreateEntity()
+scene.SetComponent(cloudEntity,"behaviour","scripts/cloud.lua")
+scene.SetComponent(cloudEntity,"tag","cloudManager")
 
-dofile("scripts/coin/coinManager.lua")
+--dofile("scripts/coin/coinManager.lua")
 -- dofile("scripts/hat.lua")
 -- dofile("scripts/editingBlock.lua")
 coin = require("scripts.coin.editCoinManager")

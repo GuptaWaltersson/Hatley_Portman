32c50961f387764ac3e87fa7acca59ff63fe1e80
TothOliver <olivertoth.in@gmail.com>
2025-05-19 11:27:11 +0200

    coinManager fixed

--- Application/scripts/coin/coin.lua
+++ Application/scripts/coin/coin.lua
@@ -1,5 +1,13 @@
 local coin = {}
 
+function coin.OnCreate(self)
+	print("Lua Coin created!")
+end
+
+function coin.OnUpdate(self, delta)
+	-- Update logic for the coin can be added here if needed
+end
+
 function coin.OnCollision(self, other)
 	print("Lua Coin collected!")
 	
--- Application/scripts/scene.lua
+++ Application/scripts/scene.lua
@@ -6,12 +6,12 @@ scene.SetComponent(hatEntity,"behaviour","scripts/hat.lua")
 local playerEntity = scene.CreateEntity()
 scene.SetComponent(playerEntity, "behaviour", "scripts/player.lua")
 
-coinManager = require("scripts.coin.coinManager")
-coinManager.CreateSceneManager()
-
 local cloudEntity = scene.CreateEntity()
 scene.SetComponent(cloudEntity,"behaviour","scripts/cloud.lua")
 scene.SetComponent(cloudEntity,"tag","cloudManager")
 
 block = require("scripts.editingBlock")
 block.Ground()
+
+coinManager = require("scripts.coin.coinManager")
+coinManager.CreateSceneManager()
